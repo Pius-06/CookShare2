9318cad1a1db4d32d4f109fb7d3a1dabbf3006ae
Pius-06 <tebbe.pius7@gmail.com>
2026-02-15 18:09:54 +0100

    Hello World!

--- backend/src/main/java/de/pius/cookshare/CookshareApplication.java
+++ backend/src/main/java/de/pius/cookshare/CookshareApplication.java
@@ -8,6 +8,7 @@ public class CookshareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CookshareApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
