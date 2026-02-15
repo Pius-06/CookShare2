971ae8d3084d48e29aa077763a2d7588a6fcc1e2
Pius-06 <tebbe.pius7@gmail.com>
2026-02-15 17:08:32 +0100

    add backend

---
+++ backend/src/test/java/de/pius/cookshare/CookshareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pius.cookshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
