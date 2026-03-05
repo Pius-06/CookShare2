2b93299d72a1c8fc746461088367e93504045be0
Pius-06 <tebbe.pius7@gmail.com>
2026-03-05 22:43:11 +0100

    RecipeController erstellt ohne Routen

---
+++ backend/src/main/java/de/pius/cookshare/recipe/recipe/RecipeController.java
@@ -0,0 +1,10 @@
+package de.pius.cookshare.recipe.recipe;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/recipes")
+public class RecipeController {
+    
+}
