619dc5ce6469a5f2e76c361b37b2f49202a2cfcc
Pius-06 <tebbe.pius7@gmail.com>
2026-03-09 12:19:21 +0100

    Berechtigung zum Löschen von Benutzern, um die Rolle ADMIN zu ermöglichen aktualisiert

--- backend/src/main/java/de/pius/cookshare/user/UserController.java
+++ backend/src/main/java/de/pius/cookshare/user/UserController.java
@@ -4,18 +4,15 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import de.pius.cookshare.user.dto.UserRequestDTO;
 import de.pius.cookshare.user.dto.UserResponseDTO;
 import de.pius.cookshare.user.dto.UserUpdateDTO;
 import jakarta.validation.Valid;
@@ -53,7 +50,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("#id == authentication.principal.id")
+    @PreAuthorize("#id == authentication.principal.id or hasRole('ADMIN')")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
 
         userService.deleteUser(id);
