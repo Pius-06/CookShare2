c195b6db0bf4796c9613f6129aee2f7b7b0f4379
Pius-06 <tebbe.pius7@gmail.com>
2026-03-12 09:34:34 +0100

    Query für findPublicRecipesByAuthorId hinzugefügt

--- backend/src/main/java/de/pius/cookshare/recipe/recipe/RecipeRepository.java
+++ backend/src/main/java/de/pius/cookshare/recipe/recipe/RecipeRepository.java
@@ -2,11 +2,24 @@ package de.pius.cookshare.recipe.recipe;
 
 import java.util.Set;
 
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    
-    Set<Recipe> findByAuthorId(Long userId);
+
+    Set<Recipe> findByAuthorId(Long authorId);
+
+    @Query("""
+            SELECT r
+            FROM Recipe r
+            WHERE r.authorId = ?1
+            AND r.isPublic = true
+            """)
+    Set<Recipe> findPublicRecipesByAuthorId(Long authorId);
+
+    Page<Recipe> findAll(Pageable pageable);
 }
